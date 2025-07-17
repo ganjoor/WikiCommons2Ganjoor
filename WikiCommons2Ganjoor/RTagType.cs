0e31ff500ceccf3d8536e2877c76fa309f2cdc78
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2025-07-17 15:24:01 +0330

    importing some models

---
+++ WikiCommons2Ganjoor/RTagType.cs
@@ -0,0 +1,45 @@
+﻿namespace WikiCommons2Ganjoor
+{
+    /// <summary>
+    /// نوع ویژگی
+    /// </summary>
+    public enum RTagType
+    {
+        /// <summary>
+        /// عادی
+        /// </summary>
+        Ordinary = 0,
+        /// <summary>
+        /// پیوند
+        /// </summary>
+        Link = 1,
+        /// <summary>
+        /// جستجو
+        /// </summary>
+        Search = 2,
+        /// <summary>
+        /// پیوند و جستجو
+        /// </summary>
+        LinkSearch = 3,
+        /// <summary>
+        /// متن چپ به راست
+        /// </summary>
+        LeftToRightText = 4,
+        /// <summary>
+        /// بدون مقدار
+        /// </summary>
+        Binary = 5,
+        /// <summary>
+        /// عنوان در فهرست
+        /// </summary>
+        TitleInContents = 6,
+        /// <summary>
+        /// IIIF
+        /// </summary>
+        IIIF = 7,
+        /// <summary>
+        /// لینک در نسخه - برای نمایش انجامه‌ها
+        /// </summary>
+        LinkInMaster = 8,
+    }
+}
